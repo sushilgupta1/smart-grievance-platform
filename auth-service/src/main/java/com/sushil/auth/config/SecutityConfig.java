310d6fa6d05d9af6a65d0bbf10c37771b8a614f4
Sushil Kumar Gupta <gsushil369@gmail.com>
2026-03-28 03:02:35 +0530

    feat: add auth test API and disable security temporarily

---
+++ auth-service/src/main/java/com/sushil/auth/config/SecutityConfig.java
@@ -0,0 +1,18 @@
+package com.sushil.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecutityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception
+	{
+		http.csrf(csrf->csrf.disable()).authorizeHttpRequests(auth->auth.anyRequest().permitAll());
+		return http.build();
+	}
+
+}

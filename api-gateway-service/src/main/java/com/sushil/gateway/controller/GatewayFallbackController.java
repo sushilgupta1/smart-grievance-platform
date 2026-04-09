ad86bf6a721f553207054d970a78971ea6c7ada0
Sushil Kumar Gupta <gsushil369@gmail.com>
2026-04-10 00:00:51 +0530

    feat(resillience):  injected spring boot actuator monitoring and Gateway Resillience4j circuit breakers

--- api-gateway-service/src/main/java/com/sushil/gateway/controller/GatewayFallbackController.java
+++ api-gateway-service/src/main/java/com/sushil/gateway/controller/GatewayFallbackController.java
@@ -1,5 +1,22 @@
 package com.sushil.gateway.controller;
 
-public class GatewayFallbackController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/fallback")
+public class GatewayFallbackController {
+	
+    @RequestMapping("/auth")
+    public ResponseEntity<String> authFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("Authentication Service is highly degraded. Please try again later.");
+    }
+    @RequestMapping("/grievance")
+    public ResponseEntity<String> grievanceFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("Grievance Service is experiencing high latency. Circuit breaker is active.");
+    }
 }
